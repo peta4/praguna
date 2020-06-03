9045b467a64d524b6fc8090242e036d1d4ef7e3a
peta4 <petapeta@post.cz>
2020-06-03 02:08:18 +0200

    uprava parallaxu 2

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Title from './components/Title/Title'
-import Logo from './img/u-praguny-logo.png'
+
 
 function Header() {
     let headerStyles = {textAlign:'center', marginTop: '130px'}
